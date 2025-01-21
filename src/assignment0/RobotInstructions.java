dc78bf3b08411655fc7e0450920624d6cfb27ad3
miguelibarra <miguelibarra@localhost>
2025-01-21 12:33:33 -0600

    My first assignment :))

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,19 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
+		robot.forward(400);
 		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(120);
+		robot.turnRight(135);
+		robot.forward(200);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(200);
+		robot.turnRight(135);
+		robot.forward(120);
+		robot.turnLeft(90);
+		robot.forward(400);
+		robot.turnRight(90);
+		robot.forward(40);
 	}
 
 }
